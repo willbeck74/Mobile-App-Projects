13a6a42da38533e151bccc7d9c1d8dfd5d9b5538
beckww <beckww@miamioh.edu>
2021-09-06 11:50:28 -0400

    Grid Layouts

---
+++ GridXAML/GridXAML/GridXAML/GridPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace GridXAML
+{
+    public partial class XAML : ContentPage
+    {
+        public XAML()
+        {
+            InitializeComponent();
+        }
+    }
+}
