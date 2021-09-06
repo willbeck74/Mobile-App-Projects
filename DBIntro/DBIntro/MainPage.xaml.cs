9d2f3b8e21342c846fe00e12a6869f9a7dd44d62
beckww <beckww@miamioh.edu>
2021-09-06 11:48:05 -0400

    Database Basics

---
+++ DBIntro/DBIntro/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using SQLite;
+using Xamarin.Essentials;
+
+namespace DBIntro
+{
+    public partial class MainPage : ContentPage
+    {
+        SQLiteConnection conn;
+        public MainPage()
+        {
+            InitializeComponent();
+
+            string libFolder = FileSystem.AppDataDirectory;
+            string fname = System.IO.Path.Combine(libFolder, "Personnel.db");
+            conn = new SQLiteConnection(fname);
+            conn.CreateTable<User>();
+            lv.ItemsSource = conn.Table<User>().ToList();
+
+            conn.CreateTable<Person>();
+            lv2.ItemsSource = conn.Table<Person>().ToList();
+
+        }
+
+        void addUser_Clicked(System.Object sender, System.EventArgs e)
+        {
+            User newUser = new User { Username = name.Text };
+            conn.Insert(newUser);
+            lv.ItemsSource = conn.Table<User>().ToList();
+        }
+
+        void addPerson_Clicked(System.Object sender, System.EventArgs e)
+        {
+            Person newPerson = new Person
+            {
+                Name = personNameEntry.Text,
+                Gender = isFemale.IsChecked,
+                DOB = dateBirth.Date,
+                SSN = socialEntry.Text,
+                Income = Int32.Parse(incomeEntry.Text)
+            };
+            conn.Insert(newPerson);
+            lv2.ItemsSource = conn.Table<Person>().ToList();
+        }
+    }
+}
---
+++ DBIntro/DBIntro/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using SQLite;
+
+namespace DBIntro
+{
+    [Table("person")]
+    public class Person
+    {
+        [PrimaryKey, AutoIncrement, Column("_id")]
+        public int Id { get; set; }
+
+        [MaxLength(200), Unique]
+        public string Name { get; set; }
+        public bool Gender { get; set; }
+        public DateTime DOB { get; set; }
+
+        [MaxLength(15), Unique]
+        public string SSN { get; set; }
+
+        public int Income { get; set; }
+
+        public override string ToString()
+        {
+            string gender = "M";
+            if (Gender)
+                gender = "F";
+
+            return string.Format("{0} ({1}) {2} {3} {4} {5}", Name, Id, Income, gender, DOB, SSN);
+        }
+    }
+}
