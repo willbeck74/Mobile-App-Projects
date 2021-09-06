c56affe5119b41b645e2dfdb86b345b18f4f51f8
beckww <beckww@miamioh.edu>
2021-09-06 11:35:48 -0400

    Audio Exercise

---
+++ AudioExercise/AudioExercise/App.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Plugin.SimpleAudioPlayer;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AudioExercise
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        private void PlaySound(string file)
+        {
+            var assembly = typeof(App).GetTypeInfo().Assembly;
+            String thisNameSpace = "AudioExercise";
+            Stream audioStream = assembly.GetManifestResourceStream(thisNameSpace + "." + file);
+            ISimpleAudioPlayer player = CrossSimpleAudioPlayer.CreateSimpleAudioPlayer();
+            player.Load(audioStream);
+            player.Play();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ AudioExercise/AudioExercise/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Plugin.SimpleAudioPlayer;
+using Xamarin.Forms;
+
+namespace AudioExercise
+{
+    public partial class MainPage : ContentPage
+    {
+       
+        ISimpleAudioPlayer player = CrossSimpleAudioPlayer.CreateSimpleAudioPlayer();
+        public MainPage()
+        {
+            InitializeComponent();
+            Load("hello.mp3");
+        }
+        private void Load(string file)
+        {
+            var assembly = typeof(App).GetTypeInfo().Assembly;
+            String ns = "AudioExercise";
+            Stream audioStream = assembly.GetManifestResourceStream(ns + "." + file);
+            player.Load(audioStream);
+        }
+      
+
+        void Play_Clicked(System.Object sender, System.EventArgs e)
+        {
+            player.Play();
+        }
+
+        void Stop_Clicked(System.Object sender, System.EventArgs e)
+        {
+            player.Stop();
+        }
+
+        void loopSwitch_PropertyChanged(System.Object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            player.Loop = !player.Loop;
+        }
+    }
+}
