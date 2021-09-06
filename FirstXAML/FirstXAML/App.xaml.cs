6bd24a9e0b80dcfaa415e5dc41d03953d2931286
beckww <beckww@miamioh.edu>
2021-09-06 11:49:41 -0400

    Xaml Basics

---
+++ FirstXAML/FirstXAML/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FirstXAML
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MyXAMLPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ FirstXAML/FirstXAML/MyXAMLPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace FirstXAML
+{
+    public partial class MyXAMLPage : ContentPage
+    {
+        public MyXAMLPage()
+        {
+            InitializeComponent();
+        }
+        void OnCheckBoxCheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            if (checkBox.Equals("True"))
+                theLabel.Text = "Adult";
+            else
+            {
+                theLabel.Text = "";
+            }
+        }
+    }
+}
